d708c29952d4cbafeb3564b32b9267286f8a3c07
Romain Beaumont <romain.rom1@gmail.com>
2022-11-16 04:47:40 +0100

    Update usagebot.py

--- usagebot.py
+++ usagebot.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3 
 
 """
-SLURM usage tracker Discord bot by drscotthawley & rom1504
-
 Requires external file token_channel.csv to connect to Discord
 Syntax of that file should be:
 
@@ -10,43 +8,40 @@ token,channel
 <DISCORD_BOT_TOKEN>,<CHANNEL_ID>
 
 Without token & channel info, this code will print SLURM cluster usage info to stdout.
-
-
-initial code by rom1504 at https://gist.github.com/rom1504/09bcafe41605be441dbbbfaaf500f42e
 """
 
-import discord
-from discord.ext import tasks
-import json
-import pandas as pd
-import subprocess
-import sys
+
 import pandas as pd
 from pssh.clients import ParallelSSHClient
 from io import StringIO
+import json
+import subprocess
 
 def compute_power_per_node():
     m = 465
     #m = 3
     hosts = ["gpu-st-p4d-24xlarge-"+str(i) for i in range(1,m)]
-    client = ParallelSSHClient(hosts, timeout=10)
+    client = ParallelSSHClient(hosts, timeout=10, pool_size=500)
 
-    output = list(client.run_command('bash -c "for i in 1 .. 5 ; do nvidia-smi --query-gpu="power.draw"  --format=csv,noheader,nounits ; sleep 1; done"', stop_on_errors=False))
+    output = list(client.run_command('bash -c "for i in {1..5} ; do nvidia-smi --query-gpu=index,power.draw  --format=csv,nounits ; echo  ; sleep 1; done"', stop_on_errors=False))
 
 
 
-    node_to_power_usage = {}
+    node_gpu_to_power_usage = {}
     for o in output:
-        df = pd.read_csv(StringIO("\n".join(o.stdout)), header=None)
-        mm = df.mean()
-        if len(mm) == 0:
-            m = 0
-        else:
-            m = float(mm[0])
-        node_to_power_usage[o.host] = m
-
-    return node_to_power_usage
-
+        c = "\n".join(o.stdout)
+        cs = c.split("\n\n")
+        df = None
+        for cc in cs:
+            dd = pd.read_csv(StringIO(cc))
+            if df is None:
+                df = dd
+            else:
+                df[" power.draw [W]"] += dd[" power.draw [W]"]
+        df["power"] = pd.to_numeric(df[" power.draw [W]"], errors="coerce")/5
+        for i, p in zip(df["index"], df["power"]):
+            node_gpu_to_power_usage[o.host+":"+str(i)] = p
+    return node_gpu_to_power_usage
 
 def expand_nodes(s):
         s = s.replace("gpu-st-p4d-24xlarge-", "").replace("[","").replace("]","")
@@ -63,46 +58,66 @@ def expand_nodes(s):
                 nodes.append(int(n))
         return ["gpu-st-p4d-24xlarge-"+str(n) for n in nodes]
 
-def get_msg(
-    backticks=True   # whether to add backticks for Discord formatting or not
-    ):
-    "gets a list of cluster usage from squeue and creates a text message from it"
-    enable_power_computation = False
-    if enable_power_computation:
-        node_to_power_usage = compute_power_per_node()
-    
+# looks like gpu:a100:8(IDX:0-7)
+def parse_gpu(gpu):
+    gpu = gpu[15:-1]
+    if gpu == "N/A":
+        return []
+    if "-" in gpu:
+       gpu = list(range(int(gpu[0]), 1+int(gpu[2])))
+    else:
+       gpu = [int(gpu)]
+    return gpu
+
+def backtick(msg):
+    lines = msg.split("\n")
+    final_lines= []
+    for i in range(0, len(lines), 30):
+        final_lines.append("```\n")
+        final_lines.extend(lines[i:(i+30)])
+        final_lines.append("```\n\n")
+    return "\n".join(final_lines)
+
+def get_msg():
+
+    node_gpu_to_power_usage = compute_power_per_node()
+
     a = json.loads(subprocess.check_output(['squeue','--json']).decode("utf8"))
     df = pd.DataFrame(a["jobs"])
-    
-    if enable_power_computation:
-        sums = []
-        for nodes in df["nodes"]:
-            fnodes = expand_nodes(nodes)
-            power_usages = [node_to_power_usage[n] if n in node_to_power_usage else 0 for n in fnodes]
-            sum_pw = sum(power_usages)
-            sums.append(sum_pw)
 
-        df["sum_power_usage"] = sums
+
+    sums = []
+    gpu_counts = []
+    for gpus,nodes in zip(df["gres_detail"], df["nodes"]):
+       fnodes = expand_nodes(nodes)
+       power_usage = 0
+       gpu_count = 0
+       for gpu,node in zip(gpus,fnodes):
+          gpu = parse_gpu(gpu)
+          gpu_count += len(gpu) 
+          for g in gpu:
+              power_usage += node_gpu_to_power_usage[node+":"+str(g)] if node+":"+str(g) in node_gpu_to_power_usage else 0
+       sums.append(power_usage)
+       gpu_counts.append(gpu_count)
+
+    df["sum_power_usage"] = sums
+    df["gpu_count"] = gpu_counts
 
     a = json.loads(subprocess.check_output(['sinfo','--json']).decode("utf8"))
-    num_idles = sum([1 for a in  a['nodes'] if a['state'] == 'idle' and 'gpu' in a['name'] and 'POWERED_DOWN' not in a['state_flags']])
+
+    num_idles = sum([int(a['gres'][-1]) - len(parse_gpu(a['gres_used'])) for a in  a['nodes'] if 'gpu' in a['name'] and 'POWERED_DOWN' not in a['state_flags']])
 
     preemptible_accounts = [e[0] for e in [l.split("|") for l in subprocess.check_output(['sacctmgr', 'list', '--parsable', 'Account']).decode("utf8").split("\n")] if len(e) >= 3 and e[2] == "root"]
 
     def group_per_user_name(df):
-        if enable_power_computation:
-            cols = ["node_count", "sum_power_usage"]
-        else:
-            cols = ["node_count"]
-        g = df.groupby(["account", "user_name"]).sum()[cols]
-        if enable_power_computation:
-            g["average_power_usage"] = g["sum_power_usage"] / g["node_count"]
-            g["gpu_efficiency"] = g["average_power_usage"] / 405.0 * 100
-            g = g[["node_count","gpu_efficiency"]]
-        g = g.sort_values("node_count")
-        g = g.round(0)
+        s = df.groupby(["account", "user_name"]).sum()
+        g = s[["gpu_count","sum_power_usage"]]
+        g["average_power_usage"] = g["sum_power_usage"] / g["gpu_count"]
+        g["gpu_efficiency"] = g["average_power_usage"] / 405.0 * 100  
+        g["gpu_efficiency"] = g["gpu_efficiency"].fillna(0).astype('int')
+        g = g[["gpu_count","gpu_efficiency"]]
+        g = g.sort_values("gpu_count")
         return str(g)
-
 
     df = df[df["partition"] == "gpu"]
     running = df[df["job_state"] == "RUNNING"]
@@ -110,55 +125,67 @@ def get_msg(
     preemptible = running[running["account"].isin(preemptible_accounts)]
     non_preemptible = running[~running["account"].isin(preemptible_accounts)]
 
-    pending_count = sum(pending["node_count"].values)
-    preemptible_count = sum(preemptible["node_count"].values)
-    non_preemptible_count = sum(non_preemptible["node_count"].values)
+    pending_count = sum(pending["gpu_count"].values)
+    preemptible_count = sum(preemptible["gpu_count"].values)
+    non_preemptible_count = sum(non_preemptible["gpu_count"].values)
 
-    msg = "```\n" if backticks else ""
-    msg += "Pending:\n"+group_per_user_name(pending)+"\n\n"
-    msg += "Preemptible:\n"+group_per_user_name(preemptible)+"\n\n"
-    msg += "Non-preemptible:\n"+group_per_user_name(non_preemptible)+"\n\n"
-    msg += f"Idle: {num_idles} nodes\n"
-    msg += f"Pending count: {pending_count} nodes\n"
-    msg += f"Preemptible count (these jobs will be killed if needed by non preemtible): {preemptible_count} nodes\n"
-    msg += f"Non pre emptible count: {non_preemptible_count} nodes\n"
-    msg = msg+"\n```" if backticks else msg
+    group1 = ""
+    group1 += "Pending:\n"+group_per_user_name(pending)+"\n\n"
+    group1 += "Preemptible:\n"+group_per_user_name(preemptible)+"\n\n"
+
+    group2 = ""
+    group2 += "Non-preemptible:\n"+group_per_user_name(non_preemptible)+"\n\n"
+
+    group3 = ""
+    group3 += f"Idle: {num_idles} gpus\n"
+    group3 += f"Pending count: {pending_count} gpus\n"
+    group3 += f"Preemptible count (these jobs will be killed if needed by non preemtible): {preemptible_count} gpus\n"
+    group3 += f"Non pre emptible count: {non_preemptible_count} gpus\n"
+
+    msg = backtick(group1)+backtick(group2)+backtick(group3)
     return msg
 
 
+discord = False
 
-# Read Discord bot token and channel id from external file
-try:
-    info_df = pd.read_csv('token_channel.csv')
-    token = info_df['token'][0]
-    channel_id = info_df['channel'][0]
-    print("Discord authorization info found.")
-    print("  token =",token)
-    print("  channel_id =",channel_id)
-    print("Proceeding.")
-except:
-    print("No Discord credentials found. Here's a printout:\n")
-    print(get_msg(backticks=False))
-    sys.exit(1)                   # might as well just stop here
-
-
-# Initialize bot client object
-client = discord.Client()
-
-
-# Setup background task 
-@tasks.loop(hours=12)
-async def my_background_task():
-    """A background task that gets invoked every __ hours."""
-    channel = client.get_channel(channel_id) 
-    await channel.send(get_msg())
-    
-@my_background_task.before_loop
-async def my_background_task_before_loop():
-    await client.wait_until_ready()
-
-my_background_task.start()
+if discord:
+    import discord
+    from discord.ext import tasks
+    # Read Discord bot token and channel id from external file
+    try:
+        info_df = pd.read_csv('token_channel.csv')
+        token = info_df['token'][0]
+        channel_id = info_df['channel'][0]
+        print("Discord authorization info found.")
+        print("  token =",token)
+        print("  channel_id =",channel_id)
+        print("Proceeding.")
+    except:
+        print("No Discord credentials found. Here's a printout:\n")
+        print(get_msg())
+        sys.exit(1)                   # might as well just stop here
 
 
-# Run the bot
-client.run(token)
+    # Initialize bot client object
+    client = discord.Client()
+
+
+    # Setup background task 
+    @tasks.loop(hours=12)
+    async def my_background_task():
+        """A background task that gets invoked every __ hours."""
+        channel = client.get_channel(channel_id) 
+        await channel.send(get_msg())
+        
+    @my_background_task.before_loop
+    async def my_background_task_before_loop():
+        await client.wait_until_ready()
+
+    my_background_task.start()
+
+
+    # Run the bot
+    client.run(token)
+else:
+    msg = get_msg()
+    print(msg)
